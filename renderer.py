ed095050bc9085ae5c595102c25383c251de18c9
misomisa <misasucha98@gmail.com>
2024-07-16 21:07:26 +0200

    starting with jinja templates

---
+++ renderer.py
@@ -0,0 +1,12 @@
+from jinja2 import Template, Environment, FileSystemLoader
+
+# load templates folder to environment (security measure)
+env = Environment(loader=FileSystemLoader('jinja_templates'))
+
+# load the `index.jinja` template
+index_template = env.get_template('index.html')
+output_from_parsed_template = index_template.render()
+
+# write the parsed template
+with open("renders/index.html", "w") as chap_page:
+    chap_page.write(output_from_parsed_template)
